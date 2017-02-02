83097fb10698300296460269b2fce4c7791ea787
rodrigo-delgado <rod.delgado85@gmail.com>
2017-02-02 21:30:40 +1300

    add process env into webpack

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,11 +2,11 @@ import firebase from 'firebase'
 
 try {
   var config = {
-    apiKey: "AIzaSyCbTAIWKJcKDXzDVMew1WzpAGfJo7-YyzM",
-    authDomain: "rodrigo-todo-app.firebaseapp.com",
-    databaseURL: "https://rodrigo-todo-app.firebaseio.com",
-    storageBucket: "rodrigo-todo-app.appspot.com",
-    messagingSenderId: "652312729478"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.SENDER_ID
   };
   firebase.initializeApp(config)
 } catch (e) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const webpack = require('webpack')
 const path = require('path')
+const envFile = require('node-env-file')
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'))
+} catch (e) {
+
+}
 
 module.exports = {
   entry: [
@@ -14,6 +23,21 @@ module.exports = {
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+        SENDER_ID: JSON.stringify(process.env.SENDER_ID)
+      }
     })
   ],
   output: {
@@ -54,5 +78,5 @@ module.exports = {
       path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
-  devtool: 'cheap-module-eval-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 }
